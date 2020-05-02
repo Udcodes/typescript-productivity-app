bcec0de6a616be2c4e5d0155580c52e4cff75a3b
Udcodes <blessingudochi@gmail.com>
2020-05-02 23:09:01 +0100

    chore: adds webpack dependencies

---
+++ src/app.ts
@@ -0,0 +1,40 @@
+// function add(n1: number, n2: number, showResult: boolean, phrase: string) {
+//   const result = n1 + n2;
+
+//   if (showResult) {
+//     console.log(phrase + result);
+//   } else {
+//     return result;
+//   }
+// }
+
+// const number1 = 5;
+// const number2 = 9.7;
+// const printResult = true;
+// const resultPhrase = "Result is: ";
+
+// add(number1, number2, printResult, resultPhrase);
+
+// const person: {
+//   name: string;
+//   age: number;
+//   hobbies: string[];
+//   role: [number, string];
+// } = {
+//   name: "Udochi",
+//   age: 27,
+//   hobbies: ["TV", "Blogs"],
+//   role: [20, "Admin"],
+// };
+
+// for (const hobby of person.hobbies) {
+//   console.log(hobby);
+// }
+
+function startGame() {
+  //starting a new game
+  var messagesElement = document.getElementById("messages");
+  messagesElement!.innerText = "Welcome to MultiMath! Starting new game...";
+}
+
+document.getElementById("startGame")!.addEventListener("click", startGame);
